110c8d7ec867fd7926bb2671b5fa720def44389a
Sofia <fia.borg@hotmail.com>
2022-10-28 14:47:26 +0200

    ok

--- src/API.ts
+++ src/API.ts
@@ -16,12 +16,3 @@ export const fetchQuestions = async (): Promise<QuestionsState[]> => {
 		answers: [...question.incorrect_answers, question.correct_answer],
 	}));
 };
-
-// export const fetchQuestions = async (): Promise<QuestionState[]> => {
-// 	const url = `https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple`;
-// 	const data = await (await fetch(url)).json();
-// 	return data.results.map((question: Question) => ({
-// 		...question,
-// 		answers: shuffle([...question.incorrect_answers, question.correct_answer]),
-// 	}));
-// };
